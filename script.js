9c3f5c47c631e7eeb7ac93cab85aaf2c39ffe9e6
vinson <dagger1245653@gmail.com>
2021-06-19 16:51:07 -0700

    Added some more features and progress on the new book button

--- script.js
+++ script.js
@@ -7,19 +7,25 @@ bookTitle.type = 'text';
 bookAuthor.type = 'text';
 bookPages.type = 'text';
 bookStatus.id = 'read-status';
+bookTitle.classList.add('newBook');
+bookAuthor.classList.add('newBook');
+bookPages.classList.add('newBook');
+bookStatus.classList.add('newBook');
 //Add options for dropdown
 let bookRead = document.createElement('option');
 bookRead.value = 'read';
 bookRead.text = 'read';
 bookStatus.appendChild(bookRead);
 let bookNotRead = document.createElement('option');
-bookNotRead.value = 'not-read';
+bookNotRead.value = 'not read';
 bookNotRead.text = 'not read';
 bookStatus.appendChild(bookNotRead);
 
 //Submit button
 let submit = document.createElement('button');
 submit.type = 'submit';
+submit.textContent = 'Submit';
+submit.style.cssText = 'margin-top: 20px;'
 //Div selectors
 let addBook = document.querySelector('#add-book');
 let addBookUI = document.querySelector('#submit-form')
@@ -43,6 +49,7 @@ function addBookToLibrary() {
 }
 
 function bookLoop() {
+    displayBooks.innerHTML = '';
     for (let objs of myLibrary) {
         let paragraph = document.createElement('p');
         paragraph.textContent = objs
@@ -50,22 +57,52 @@ function bookLoop() {
     }
 }
 
+let titlePrompt = document.createElement('p');
+let authorPrompt = document.createElement('p');
+let pagesPrompt = document.createElement('p');
+let statusPrompt = document.createElement('p');
 addBook.addEventListener('click', () => {
-    addBookUI.style.cssText = 'background-color: yellow; position: absolute;';
+    addBookUI.style.cssText = `background-color: #629460; position: absolute; 
+    width: 500px; height: 500px; display: flex; 
+    flex-direction: column;align-items: center;
+    margin-left: auto;margin-right: auto;margin-top: 70px;
+    left: 0; right: 0; text-align: center;`;
+    titlePrompt.textContent = 'Title of Book:';
+    addBookUI.appendChild(titlePrompt);
+    bookTitle.style.cssText = 'margin: 20px 0px 10px 0px;'
     addBookUI.appendChild(bookTitle);
+    authorPrompt.textContent = 'Author of Book:';
+    addBookUI.appendChild(authorPrompt);
+    bookAuthor.style.cssText = 'margin: 20px 0px 10px 0px;'
     addBookUI.appendChild(bookAuthor);
+    pagesPrompt.textContent = 'Pages of Book:';
+    addBookUI.appendChild(pagesPrompt);
+    bookPages.style.cssText = 'margin: 20px 0px 10px 0px;'
     addBookUI.appendChild(bookPages);
+    statusPrompt.textContent = 'Current Book Status:';
+    addBookUI.appendChild(statusPrompt);
+    bookStatus.style.cssText = 'margin: 20px 0px 10px 0px;'
     addBookUI.appendChild(bookStatus);
     addBookUI.appendChild(submit);
 });
 
 submit.addEventListener('click', () => {
-    addBookUI.style.cssText = 'background-color: white';
+    addBookUI.style.cssText = '';
+    let title = bookTitle.value;
+    let author = bookAuthor.value;
+    let pages = bookPages.value;
+    let status = bookStatus.value;
     addBookUI.removeChild(bookTitle);
     addBookUI.removeChild(bookAuthor);
     addBookUI.removeChild(bookPages);
     addBookUI.removeChild(bookStatus);
     addBookUI.removeChild(submit);
+    let createBook = new book(title, author, pages, status);
+    myLibrary.push(createBook.info());
+    bookLoop();
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookPages.value = '';
 });
 
 let Ulysses = new book('Ulysses', 'James Joyce', '542', 'not read');
